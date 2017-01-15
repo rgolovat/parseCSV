245fb065f85e9410e7790161ba2435f32973fa48
rgolovatyi <rgolovatyi@lohika.com>
2017-01-16 00:13:18 +0200

    first commit

---
+++ csvParse/UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace csvParse
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            csvParse.parseCsv();
+
+            var dic = csvParse.parseCsv();
+
+            for (int i = 0; i < dic.Count; i++)
+            {
+                if (i.Equals(dic.Count) && dic[i].Substring(0, dic[i].IndexOf("/")).Equals(dic[i + 1].Substring(0, dic[i + 1].IndexOf("/")))
+                    && dic[i].Substring(0, dic[i].IndexOf("/")).Equals(dic[i + 2].Substring(0, dic[i + 2].IndexOf("/")))
+                    && dic[i].Substring(0, dic[i].IndexOf("/")).Equals(dic[i + 3].Substring(0, dic[i + 3].IndexOf("/")))
+                    && dic[i].Substring(0, dic[i].IndexOf("/")).Equals(dic[i + 4].Substring(0, dic[i + 4].IndexOf("/"))))
+                {
+                    dic.Add(dic[i]);
+                }
+                else if (i.Equals(dic.Count) && dic[i + 1].Substring(0, dic[i + 1].IndexOf("/")).Equals(dic[i + 2].Substring(0, dic[i + 2].IndexOf("/")))
+                    && dic[i + 1].Substring(0, dic[i + 1].IndexOf("/")).Equals(dic[i + 3].Substring(0, dic[i + 3].IndexOf("/")))
+                    && dic[i + 1].Substring(0, dic[i + 1].IndexOf("/")).Equals(dic[i + 4].Substring(0, dic[i + 4].IndexOf("/"))))
+                {
+                    dic.Add(dic[i + 1]);
+                }
+                else if (i.Equals(dic.Count) && dic[i + 2].Substring(0, dic[i + 2].IndexOf("/")).Equals(dic[i + 3].Substring(0, dic[i + 3].IndexOf("/")))
+                    && dic[i + 2].Substring(0, dic[i + 2].IndexOf("/")).Equals(dic[i + 4].Substring(0, dic[i + 4].IndexOf("/"))))
+                {
+                    dic.Add(dic[i + 2]);
+                }
+                else if (i.Equals(dic.Count) && dic[i + 3].Substring(0, dic[i + 3].IndexOf("/")).Equals(dic[i + 4].Substring(0, dic[i + 4].IndexOf("/"))))
+                {
+                    dic.Add(dic[i + 3]);
+                }
+               
+            }
+            dic.Reverse();
+            var k = dic.Count;
+
+            using (var file = File.CreateText("test.csv"))
+            {
+                foreach (var arr in dic)
+                {
+                    file.WriteLine(string.Join(",", arr));
+                }
+            }
+        }
+    }
+}
---
+++ csvParse/csvParse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csvParse
+{
+    public class csvParse
+    {
+        public static List<string> parseCsv()
+        {
+            var dict = File.ReadLines("VTS.csv").Select(line => line.Split(',')).SelectMany(s => s).ToList();
+
+
+ return dict;
+        }
+    }
+}
